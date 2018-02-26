8dd47808117048d95edb2991614402328f243d56
alex.xu <alex.xu@github.com>
2018-02-26 00:22:09 -0500

    Not finished hw07

--- HW07/main.c
+++ HW07/main.c
@@ -7,8 +7,27 @@
 
 int main(int argc, char *argv[]) 
 {
+	if (argc < 2) {
+		return EXIT_SUCCESS;
+	}
+	if (argc == 3) {
+		if (!strcmp(argv[2], "-I")) {
+			FILE *fpt = fopen(argv[1], "r");
+			char str[MAX_LENGTH];
+			do {
+				fscanf(fpt, "%s", str);
+				if (IsInteger(str, 0)) {
+					printf("Integer\n");
+				} else {
+					printf("Not integer\n");
+				}
+			} while (!feof(fpt));
+		} else if (!strcmp(argv[2], "-D")) {
+
+		} else if (!strcmp(argv[2], "-VID")) {
 			
-					
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
--- HW07/pe07.c
+++ HW07/pe07.c
@@ -11,21 +11,50 @@
 #ifndef INT
 bool IsInteger(char* str, int index)
 {
-
-	
+	if (str[index] == '\0') {
+		return true;
+	} else { 
+		if (isdigit(str[index])) {
+			return IsInteger(str, index + 1);
+		} else {
+			return false;
+		}
+	}
 }
 #endif
 
 #ifndef DOUBLE
-bool IsDouble(char* str, int index,bool dot)
+bool IsDouble(char* str, int index, bool dot)
 {
-	
+	if (index == 0 && str[index] == '.') {
+		return false;
+	} else if (str[index] == '\0') {
+		return true;
+	} else {
+		if (str[index] == '.' && !dot) {
+			return IsDouble(str, index + 1, true);
+		} else if (isdigit(str[index])) {
+			return IsDouble(str, index + 1, dot);
+		} else {
+			return false;
+		}
+	}
 }
 #endif
 
 #ifndef VALIDID
 bool IsValidIdentifier(char* str, int index)
 {
-	
+	if (str[index] == '\0') {
+		return true;
+	} else if (index == 0 && !(isalpha(str[index]) || str[index] == '_')) {
+		return false;
+	} else {
+		if (isalpha(str[index]) || isdigit(str[index]) || str[index] == '_') {
+			return IsValidIdentifier(str, index + 1);
+		} else {
+			return false;
+		}
+	}
 }
 #endif
